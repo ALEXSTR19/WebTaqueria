71a5812a51d68cf797702fc7e53442a093fd38d6
ALEXSTR19 <alex129t@outlook.com>
2025-11-21 10:46:54 -0600

    Proyecto base Back y Front

---
+++ taquizometro/src/main/java/com/taqueria/taquizometro/TaquizometroApplication.java
@@ -0,0 +1,13 @@
+package com.taqueria.taquizometro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaquizometroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaquizometroApplication.class, args);
+	}
+
+}
---
+++ taquizometro/src/test/java/com/taqueria/taquizometro/TaquizometroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taqueria.taquizometro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaquizometroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
